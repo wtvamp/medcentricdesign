c1351f6207203f8ca2c3f1bbe0bbd1734aa65591
Warren Thompson from QA.com <warrenjthompson@gmail.com>
2015-03-14 16:59:11 -0400

    Updating CSS and HTML for new grid layout.  Kenny, this commit is for you to review.

--- index.php
+++ index.php
@@ -26,103 +26,130 @@
 <body>
 
 <header>
-    <div class="top"></div>
-    <div class="whitespace">
-        <img alt="logo" src="http://lorempixel.com/300/100/city" id="logo"></img>
-        <div class="Slogan">
+    <!-- This is the container with the logo and phone number -->
+    <div class="logoContainer">
+        <img id="logo" alt="Med Centric Credit Logo" src="/Images/MedCentric.jpg"  />
+        <div class="slogan">
             <div>Focused On Your Health</div>
-            <div class="TOPhone">Call Today 305-555-4789</div>
+            <div class="topPhone">Call Today 305-555-4789</div>
         </div>
     </div>
-    <div style="clear:both;"></div>
-    <div class="nav">
-        <div class="nav-content">
-            <ul>
-                <li class="nav-button">
-                    <a href="Default.aspx" class="rollHome" title="Home">
-                        <span class="displace">Home</span>
-                    </a>
-                </li>
-                <li class="nav-button">
-                    <a href="Default.aspx" class="rollProducts" title="Products">
-                        <span class="displace">Products & Services & Stuff</span>
-                    </a>
-                </li>
-                <li class="nav-button">
-                    <a href="Default.aspx" class="rollWhyMed" title="Why Med Centric">
-                        <span class="displace">Why Med Centric</span>
-                    </a>
-                </li>
-                <li class="nav-button">
-                    <a href="Default.aspx" class="rollContact" title="Contact">
-                        <span class="displace">Contact Us</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    <div class="topBorder2"></div>
+
+    <!-- This is the top navigation menu -->
+    <nav>
+        <ul>
+            <li>
+                <a href="Default.aspx" title="Home">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href="Default.aspx" title="Products">
+                   Products & Services & Stuff
+                </a>
+            </li>
+            <li>
+                <a href="Default.aspx" title="Why Med Centric">
+                    <span class="displace">Why Med Centric</span>
+                </a>
+            </li>
+            <li>
+                <a href="Default.aspx" title="Contact">
+                    <span class="displace">Contact Us</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
 </header>
-<div class="Image">
-    <div class="left">
-        <img src="http://lorempixel.com/150/300/city" alt="cityL" class="align-left" />
+
+<!-- The center portion with the giant city images on each side, and will eventually have a slideshow in the middle -->
+<div class="grid mainContent">
+    <div class="col-1-3">
+        <img src="http://lorempixel.com/150/300/city" alt="Left Placeholder Image of City" />
     </div>
-    <div class="center">
-        <img src= " " alt="cityC" class="center" />
+    <div class="col-2-3">
+        JAVASCRIPT SLIDESHOW GOES HERE
     </div>
-    <div class="right">
-        <img src="http://lorempixel.com/150/300/city" alt="cityR" class="align-right" />
-    </div>
-</div>
-<div style="clear:both;"></div>
-<!--Images for lower menu-->
-<div class="container">
-    <div class="thumbnail">
-        <img src="http://lorempixel.com/160/118/city" style="float: left; width: 15%; margin-right: 3%;
-		margin-bottom: 0.5em; margin-left: 150pt;">
-        <img src="http://lorempixel.com/160/118/city" style="float: left; width: 15%; margin-right: 3%;
-		margin-bottom: 0.5em;">
-        <img src="http://lorempixel.com/160/118/city" style="float: left; width: 15%; margin-right: 3%;
-		margin-bottom: 0.5em;">
-        <img src="http://lorempixel.com/160/118/city" style="float: left; width: 15%; margin-right: 3%;
-		margin-bottom: 0.5em;">
+    <div class="col-1-3">
+        <img src="http://lorempixel.com/150/300/city" alt="Right Placeholder Image of City" />
     </div>
 </div>
-<div style="clear:both;"></div>
-<div id="footer">
-    <table class="footerTable">
-        <tbody>
-        <tr>
-            <td style="padding-top: 0px; vertical-align: middle">
-                <center>
-                    <table>
-                        <tbody>
-                        <tr>
-                            <td>
-                                <a href="Default.aspx" class="footer">Home | </a>
-                            </td>
-                            <td>
-                                <a href="Member.aspx" class="footer"> Member Login | </a>
-                            </td>
-                            <td>
-                                <a href="Enroll.aspx" class="footer"> Enroll | </a>
-                            </td>
-                            <td>
-                                <a href="Contact.aspx" class="footer"> Contact</a>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </center>
-            </td>
-        </tr>
-        </tbody>
-    </table>
-    <ul class="footerDisclaimer">
-        <li>Copyright Med Centric Credit, Inc. 2015 Version: 0.8b2.0</li>
-        <a href="/index.php?r=site/pages&amp;view=privacy">Privacy Policy</a>
-        <a href="/index.php?r=site/pages&amp;view=terms">Terms of Use</a>
-    </ul>
+
+<!-- The lower images.  We have an outer grid that divides the page into thirds. -->
+<div class="grid lowerImageMenu">
+    <div class="col-1-3">
+        &nbsp; <!-- Dummy Height -->
+    </div>
+    <div class="col-2-3">
+        <!-- The the middle column of the outer grid, we add an inner grid of five columns. -->
+        <div class="grid internalImages grid-pad">
+            <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+            <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+            <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+            <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+            <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+        </div>
+    </div>
+    <div class="col-1-3">
+        &nbsp; <!-- Dummy Height -->
+    </div>
 </div>
+
+<!-- The preferred partners images has a grid AND a navigation menu -->
+<div class="preferredPartners">
+
+    <!-- We have an outer grid that divides the page into thirds. -->
+    <div class="grid">
+        <div class="col-1-3">
+            &nbsp; <!-- Dummy Height -->
+        </div>
+        <div class="col-2-3">
+            <!-- The the middle column of the outer grid, we add an inner grid of five columns. -->
+            <div class="grid preferredPartnersImages">
+                <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+                <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+                <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+                <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+                <div class="col-1-5"><img src="http://lorempixel.com/160/118/city" /></div>
+            </div>
+        </div>
+        <div class="col-1-3">
+            &nbsp; <!-- Dummy Height -->
+        </div>
+    </div>
+
+    <!-- Then we have a navigation menu that copies the one above.  Some CSS overrides make it look slightly different -->
+    <nav>
+        <ul>
+            <li>
+                <a href="" title="Home">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li>
+                <a href="" title="Products">
+                    <span>Products & Services & Stuff</span>
+                </a>
+            </li>
+            <li>
+                <a href="" title="Why Med Centric">
+                    <span>Why Med Centric</span>
+                </a>
+            </li>
+            <li>
+                <a href="" title="Contact">
+                    <span>Contact Us</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+<!-- Footer and copyright information -->
+<footer>Copyright Med Centric Credit, Inc. 2015 Version: 0.8b3.0 |
+    <a href="/index.php?r=site/pages&amp;view=privacy">Privacy Policy</a> |
+    <a href="/index.php?r=site/pages&amp;view=terms">Terms of Use</a>
+</footer>
+
 </body>
 </html>
